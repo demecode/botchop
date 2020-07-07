937f41023d789abe6fe8517b0440f015a8ea03b7
Deme <dpk.williams@gmail.com>
2020-07-07 13:18:06 +0100

    dataset complete

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import random
+
 import nltk
 
 # punkt is a tokenizer divides a text into a list of sentences
@@ -38,15 +40,15 @@ for data_ob in data_objects['intents']:
 # and extract all words and add them to the words list
 # first we lematize each word and bring the word back to its base which is easier to train the model
 words = [lemmatizer.lemmatize(w.lower()) for w in words if w not in ignore_words]
-print(words)
+# print(words)
 
 # sorted the classes list from the data
 classes = sorted(list(set(classes)))
-print(classes)
-
-print(len(documents), "documents")
-print(len(classes), "classes")
-print(len(words), "unique lematized words")
+# print(classes)
+#
+# print(len(documents), "documents",documents)
+# print(len(classes), "classes", classes)
+# print(len(words), "unique lematized words", words)
 
 pickle.dump(words, open('words.pkl', 'wb'))
 pickle.dump(classes, open('classes.pkl', 'wb'))
@@ -54,21 +56,41 @@ pickle.dump(classes, open('classes.pkl', 'wb'))
 # build the model
 
 train = []
-output_empty = [0] * len(classes)
+output_empty = [0] * len(classes)  # 5 classes so this create an empty array of five 0s (columns)
 print(output_empty, "CRAY")
 for doc in documents:
-    bag = [] # initialize bag as empty words
-    pattern_words = doc[0] # list of tokenized words for the pattern
-    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in pattern_words] # lematize each word - make the words make sense
-    print(pattern_words, 'WOAH')
+    bag = []  # initialize bag as empty words
+    pattern_words = doc[0]  # list of tokenized words for the pattern - e.g get the patterns words for each object
+    # print (pattern_words, "PATTERN_WORDS")
+    pattern_words = [lemmatizer.lemmatize(word.lower()) for word in
+                     pattern_words]  # lematize each word - make the words make sense
+    # print(pattern_words, 'WOAH')
     # create a bag of words array with 1, if a word match found in the current pattern
     for w in words:
-        print(w, "THIS IS THE MADEEEEEEEEEE")
+        # print(words, "OVER and OVER")
+        # print(w, "THIS IS THE MADEEEEEEEEEE")
+        # so for each letter found in the patter words, we add 1 to the bag else we add a 0
         bag.append(1) if w in pattern_words else bag.append(0)
-        print(bag)
+        # print(bag)
 
         output_row = list(output_empty)
-        print(output_row)
-        output_row[classes.index(doc[1])] = 1
-        print(output_row)
+        output_row[classes.index(doc[1])] = 1  # output is a '0' for each tag and '1' for current tag (for each pattern)
+        print(output_row, 'NEVER LET ME DOWN')
+        train.append([bag, output_row])
+
+
+# shuffle the features and turn into a np.array
+random.shuffle(train)
+train = np.array(train, dtype=object)
+print(train)
+
+# now we want to create test lists using X - patterns, y - intents
+#
+train_x = list(train[:,0])
+train_y = list(train[:,1])
+print(train_x, "X DATA")
+print(train_y, "Y DATA")
+
+
+# now to create the model
 
